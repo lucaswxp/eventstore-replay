394460faa8520b004e3356d56e0dc38e0d8f94e8
Lucas Pelegrino <lucas.wxp@gmail.com>
2018-11-06 17:29:18 -0300

    inserted mongo host env var

--- mongo-database/database.go
+++ mongo-database/database.go
@@ -3,21 +3,29 @@ package myDatabase
 import (
 	"context"
 	"fmt"
+	"os"
+
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 var connection *mongo.Client
-func ConnectMongo() (*mongo.Client){
+
+func ConnectMongo() *mongo.Client {
 	if connection == nil {
 		var err error
-		connection, err = mongo.NewClient("mongodb://localhost:27017")
+		var mongoHost string
+		if mongoHost = os.Getenv("MONGO_HOST"); mongoHost == "" {
+			mongoHost = "127.0.0.1"
+		}
+
+		connection, err = mongo.NewClient("mongodb://" + mongoHost + ":27017")
 		if err != nil {
-			fmt.Printf("ERRO: %s", err)
-			return nil
+			fmt.Printf("ERRO conexão: %s", err)
+			panic(err)
 		}
 		connection.Connect(context.Background())
 		return connection
-	} else{
+	} else {
 		return connection
 	}
 }
